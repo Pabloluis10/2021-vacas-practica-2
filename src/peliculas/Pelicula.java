d9644272231a7c0d1909c0149b92e1eb4fb8bd1d
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-06-16 22:43:31 -0600

    Agregue el package peliculas y agregue la clase Pelicula.java con sus atributos y sus metodos getters y setters

---
+++ src/peliculas/Pelicula.java
@@ -0,0 +1,61 @@
+package src.peliculas;
+
+public class Pelicula {
+    
+    //Atributos
+    private int id;
+    private String nombre;
+    private int año;
+    private String categoria;
+    private boolean disponible;
+
+    //constructor
+    public Pelicula(int id, String nombre, int año, String categoria, boolean disponible){
+        this.id = id;
+        this.nombre = nombre;
+        this.año = año;
+        this.categoria = categoria;
+        this.disponible = disponible;
+    }
+
+    //getters & setters
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setNombre (String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setAño (int año){
+        this.año = año;
+    }
+
+    public void setCategoria (String categoria){
+        this.categoria = categoria;
+    }
+
+    public void setDisponible (boolean disponible){
+        this.disponible = disponible;
+    }
+
+    public int getId (){
+        return id;
+    }
+
+    public String getNombre () {
+        return nombre;
+    }
+
+    public int getAño(){
+        return año;
+    }
+
+    public String getCategoria(){
+        return categoria;
+    }
+
+    public boolean getDisponible(){
+        return disponible;
+    }
+}
